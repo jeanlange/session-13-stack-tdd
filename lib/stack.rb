ff0f06931537639e9ad72d53bd4a3e8bb3154915
Jean Lange <jean.f.lange@gmail.com>
2021-10-15 10:16:08 -0400

    added storage to the stack

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,5 +1,17 @@
 class Stack
+    # this runs every time we do Stack.new
+    # "constructor"
+    def initialize
+        # something to store the things we put in...
+        @storage = []
+    end
+
     def empty?
-        true
+        @storage.empty?
+    end
+
+    def push a_thing
+        # store it in @storage
+        @storage << a_thing
     end
 end
--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -13,4 +13,19 @@ describe Stack do
         # then - check the thing
         expect(stack_emptiness).to be_truthy
     end
+
+    it "can put something into the stack" do
+        my_stack = Stack.new
+        my_stack.push("walk the dog")
+        # then - it doesn't fail
+    end
+
+    it "stacks with stuff in them aren't empty" do
+        my_stack = Stack.new
+        my_stack.push("walk the dog")
+
+        stack_emptiness = my_stack.empty?
+
+        expect(stack_emptiness).to be false
+    end
 end
