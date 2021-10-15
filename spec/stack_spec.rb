a64b1fa2ee6f3f56aa98a5b14493dcef589d05e6
Jean Lange <jean.f.lange@gmail.com>
2021-10-15 09:54:37 -0400

    new stacks are empty

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -1,7 +1,16 @@
-# eventually require up here
+require 'stack'
 
-describe "Stack" do
+describe Stack do
     it "can do rspec" do
         expect([]).to be_truthy
+    end
+
+    it "new stacks are empty" do
+        # given - what is the initial state
+        my_stack = Stack.new
+        # when - do the thing
+        stack_emptiness = my_stack.empty?
+        # then - check the thing
+        expect(stack_emptiness).to be_truthy
     end
 end
